c4e279cbe3ff98b9b426b209bff70e1a01bcd2f9
AnnaPilat <anna-pilat@ukr.net>
2025-03-06 11:38:11 +0200

    Update index.tsx

--- src/components/layouts/DefaultBaseLayout/index.tsx
+++ src/components/layouts/DefaultBaseLayout/index.tsx
@@ -19,8 +19,7 @@ export default function DefaultBaseLayout(props) {
 
             {/* ✅ Додаємо зовнішній скрипт коректно */}
             <Script
-                async
-                src="https://stage-widget.intelswift.com/script.js?tenantId=835c3375-8bff-4f6b-94d0-dbbd537f5303&botId=67c82ae56381d68d8e0e491e&uuid=70907f9c-aaec-43dc-a0d4-7d82108c376a&end=true"
+             async src="https://stage-widget.intelswift.com/script.js?tenantId=3a64cd11-b987-407a-bfe8-7395f34ffab1&botId=67c96c606381d68d8e0e5e8a&uuid=ec143e14-19ce-49f9-992e-6113043a7bcb&end=true"
                 strategy="afterInteractive"
             />
 
@@ -30,35 +29,34 @@ export default function DefaultBaseLayout(props) {
                 strategy="afterInteractive"
                 dangerouslySetInnerHTML={{
                     __html: `
-                        window.onload = function(event) {
-                            const propsInterval = setInterval(widgetTimer, 1000);
+                        window.onload = (event) => {
+    const propsInterval = setInterval(widgetTimer, 1000);
 
-                            function widgetTimer() {
-                                const tenantId = localStorage.getItem("wws-tenant-id");
-                                const botId = localStorage.getItem("wws-bot-id");
-                                const uuid = localStorage.getItem("wws-uuid");
-                                const host = window.location.hostname;
-                                const language = navigator.language || navigator.userLanguage; 
+    function widgetTimer() {
+      const tenantId = localStorage.getItem("wws-tenant-id")
+      const botId = localStorage.getItem("wws-bot-id")
+      const uuid = localStorage.getItem("wws-uuid")
+      const host = window.location.hostname
+      const language = navigator.language || navigator.userLanguage; 
 
-                                if (
-                                    (uuid && uuid !== "undefined") && 
-                                    (tenantId && tenantId !== "undefined") && 
-                                    (botId && botId !== "undefined") && 
-                                    (host && host !== "undefined")
-                                ) {
-                                    clearInterval(propsInterval);
-                                    document.getElementById("iframeWidgetContainer")?.contentWindow.postMessage(
-                                        {
-                                            tenantId: tenantId,
-                                            botId: botId,
-                                            uuid: uuid,
-                                            host: host,
-                                            contact_language: language
-                                        }, "*"
-                                    );
-                                }
-                            }
-                        };
+      if(
+        (uuid && uuid != "undefined") && 
+        (tenantId && tenantId != "undefined") && 
+        (botId && botId != "undefined") && 
+        (host && host != "undefined")
+      ){
+        clearInterval(propsInterval);
+        document.getElementById("iframeWidgetContainer").contentWindow.postMessage( 
+        {
+          tenantId: tenantId,
+          botId: botId,
+          uuid: uuid,
+          host: host,
+          contact_language: language
+        },"*")
+      }
+    }
+  };
                     `,
                 }}
             />
